574944c7069949a13b2f08807314425a1fca1852
pm430 <open.js@gmail.com>
2022-11-08 00:31:47 +0900

    DIP 적용

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -2,15 +2,15 @@ package hello.core;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
-import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemberService;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        MemberServiceImpl memberService = new MemberServiceImpl();
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
-
 
         Member findMember = memberService.findMember(1L);
         System.out.println("new member = " + member.getName());
