16742b10c3d948d0df0fda51282e4ee04a5db89e
pm430 <open.js@gmail.com>
2022-11-12 06:44:00 +0900

    assertInstanceOf 사용

--- src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ApplicationContextBasicFindTest {
@@ -18,21 +18,21 @@ public class ApplicationContextBasicFindTest {
     @DisplayName("빈 이름으로 조회")
     void findBeanByName() {
         MemberService memberService = ac.getBean("memberService", MemberService.class);
-        assertEquals(memberService.getClass(), MemberServiceImpl.class);
+        assertInstanceOf(MemberService.class, memberService);
     }
 
     @Test
     @DisplayName("이름 없이 타입으로만 조회")
     void findBeanByType() {
         MemberService memberService = ac.getBean(MemberService.class);
-        assertEquals(memberService.getClass(), MemberServiceImpl.class);
+        assertInstanceOf(MemberService.class, memberService);
     }
 
     @Test
     @DisplayName("빈 이름으로 조회")
     void findBeanByName2() {
         MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
-        assertEquals(memberService.getClass(), MemberServiceImpl.class);
+        assertInstanceOf(MemberService.class, memberService);
     }
 
     @Test
